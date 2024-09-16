79a10d3df8930bb62bce9ac9cbaaa2ede9920ec5
Samuel Hurtado <hurtadost055@gmail.com>
2024-09-16 16:52:12 -0500

    Modulo ingenieria industrial

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 function openTab(event, tabName) {
     // Ocultar todas las pestañas
-    var i, tabinfo, tablinks;
-    tabinfo = document.getElementsByClassName("tab-info");
-    for (i = 0; i < tabinfo.length; i++) {
-        tabinfo[i].style.display = "none";
-    }
+    const tabinfo = document.querySelectorAll(".tab-info");
+    tabinfo.forEach(tab => tab.style.display = "none");
+
     // Eliminar la clase "active" de todos los enlaces de pestaña
-    tablinks = document.getElementsByClassName("tab-link");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
+    const tablinks = document.querySelectorAll(".tab-link");
+    tablinks.forEach(link => link.classList.remove("active"));
+
     // Mostrar la pestaña actual y añadir la clase "active"
     document.getElementById(tabName).style.display = "block";
-    event.currentTarget.className += " active";
+    event.currentTarget.classList.add("active");
 }
+
